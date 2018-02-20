e394517b21b58357540ca34f1bd36f2cc68c0d8c
Espen Sonneland <espen.sonneland@nmbu.no>
2018-02-20 15:36:45 +0100

    Prøver å planlegge strukturen til et potensielt lineært program

--- espen_eksperimenterer.py
+++ espen_eksperimenterer.py
@@ -15,22 +15,34 @@ _lighting = np.array([0,0,0,0,0,0,
             0,0,0,0,1,1,
             1,1,1,1,1,1,
             1,1,1,0,0,0])
-lighting = [i/sum(_lighting) for i in _lighting]
+lighting = np.array([i/sum(_lighting) for i in _lighting])
 
 _heating = np.array([9.6]*24)
-heating = [i/sum(_heating) for i in _heating]
+heating = np.array([i/sum(_heating) for i in _heating])
+
+tot_nonshiftable = lighting + heating
 
 # Shiftable sum [kWh]
 eletric_car = 9.9
+cloth_dryer = 2.5
+laundry_machine = 1.94
+dishwasher = 1.44
+tot_shiftable = eletric_car + cloth_dryer + laundry_machine + dishwasher
 
 # brukes til å finne summen av vektoren
 ones = np.ones((1,24))
 
 # RTP [NOK/kWh]
-price = np.array([0.5, 0.5, 0.5, 0.5, 0.5, 1,
+tou_price = np.array([0.5, 0.5, 0.5, 0.5, 0.5, 1,
          1, 1, 1, 0.5, 0.5, 0.5,
          0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
          0.5, 0.5, 0.5, 0.5, 0.5, 0.5])
+max_kwh = 4 # max usage pr hour
 
+price_with_nonshiftables = tot_nonshiftable*tou_price
 
-linprog(price, A_eq=ones, b_eq=eletric_car)
+dayly_usage = linprog(price_with_nonshiftables,
+                      A_eq=ones,
+                      b_eq=tot_shiftable,
+                      bounds=(0,max_kwh))
+print(dayly_usage)
