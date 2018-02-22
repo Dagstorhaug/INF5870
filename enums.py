521670e097b0fc17eada3dd99fda3522b23f9a29
Paul Joakim Andreassen <pauljoa@live.no>
2018-02-22 16:40:56 +0100

    Enum class added

---
+++ enums.py
@@ -0,0 +1,25 @@
+from enum import Enum
+class Appliance(Enum):
+    Lighting = 0
+    Heating = 1
+    Refrigerator_Freezer = 2
+    Stove = 3
+    Tv = 4
+    Computer = 5
+    Dishwasher = 6
+    LaundryMachine = 7
+    ClothDryer = 8
+    EV = 9
+    CoffeeMaker = 10
+    CeilingFan = 11
+    HairDryer = 12
+    Toaster = 13
+    Microwave = 14
+    Router = 15
+    CellphoneCharger = 16
+    ClothIron = 17
+    SeparateFreezer = 18
+    
+
+
+
