db87d71b274ecd190e1fb172f61bd385598920c6
Dag Storhaug <dag.storhaug@nmbu.no>
2018-03-20 12:11:53 +0100

    no message

--- OneDay.py
+++ OneDay.py
@@ -1,8 +1,60 @@
 # -*- coding: utf-8 -*-
 
-"""
-
-"""
 
 __author__ = 'Dag Stabell Storhaug'
-__email__ = 'dag.storhaug@nmbu.no'
+
+import random
+import numpy as np
+
+"""This function runs one day of consumption of a house or a selection of houses.
+Contains all values needed to simulate one day.
+
+Parameters
+----------
+nr_houses: int
+    Number of houses in simulation
+x_vectors: np.array
+    Array containing all appliances
+seed: int
+    seed used for random functions
+"""
+def one_day(nr_houses, x_vectors, seed=12345, rtp=False):
+    random.seed(seed)
+
+    #static pricing scheme
+    if not rtp:
+        price = np.array([
+            0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
+            0.5, 0.5, 0.5, 0.5, 1, 1, 1, 1, 0.5, 0.5, 0.5
+        ])
+    #real time pricing scheme, as determined by random numbers
+    elif rtp:
+        price = np.array([
+            random.randint(0.2, 1), random.randint(0.2, 1),
+            random.randint(0.2, 1), random.randint(0.2, 1),
+            random.randint(0.2, 1), random.randint(0.2, 1),
+            random.randint(0.2, 1), random.randint(0.2, 1),
+            random.randint(0.2, 1), random.randint(0.2, 1),
+            random.randint(0.2, 1), random.randint(0.2, 1),
+            random.randint(0.2, 1), random.randint(0.2, 1),
+            random.randint(0.2, 1), random.randint(0.2, 1),
+            random.randint(0.2, 1), random.randint(0.8, 2),
+            random.randint(0.8, 2), random.randint(0.8, 2),
+            random.randint(0.8, 2), random.randint(0.2, 1),
+            random.randint(0.2, 1), random.randint(0.2, 1)
+        ])
+
+    #When all parameters are set, create houses
+    houses = np.zeros(nr_houses)
+    for i in range(nr_houses):
+        house = House() #TODO: sett inn parametre for House
+        houses[i] = house
+
+#Constructs the app constraints matrix, 'apps' is a list of Appliance objects
+def construct_app_constraints(apps):
+    app_constraints = np.zeros((len(apps)*24, (len(apps)*24)+24))
+    i = 0
+    for appliance in apps:
+        app_constraints[i] = [1 for _ in np.linspace(i*24, (i+1)*23, 24)]
+
+
--- Plotting.py
+++ Plotting.py
@@ -15,12 +15,23 @@ plt.style.use('seaborn-colorblind')
 
 #Creates a stacked bargraph. putting the values of the np.array 'nonshift' at
 # the bottom and the values of np.array 'shift' on top.
-def consumption_plot(shift, nonshift):
+def consumption_plot(shift, nonshift, shiftnames, nonshiftnames, price):
     f, (consumptionfig) = plt.subplots(1, 1)
-    bins = np.arange(0, len(shift))
+    bins = np.arange(0, len(shift[0]))
     width = 0.60
-    p1 = consumptionfig.bar(bins, nonshift, width)
-    p2 = consumptionfig.bar(bins, shift, width, bottom=nonshift)
+    bottom = np.zeros(len(shift[0]))
+
+    #iterate over shiftable and nonshiftable appliances to create stacked
+    # bars for the chart.
+    for i in range(len(nonshift)):
+        consumptionfig.bar(bins, nonshift[i], width=width, bottom=bottom,
+                           label=nonshiftnames[i])
+        bottom = np.add(bottom, nonshift[i])
+    for i in range(len(shift)):
+        consumptionfig.bar(bins, shift[i], width=width, bottom=bottom,
+                           label=shiftnames[i])
+        bottom = np.add(bottom, shift[i])
+
 
     consumptionfig.set(
         title='Consumption of households',
@@ -29,8 +40,8 @@ def consumption_plot(shift, nonshift):
         ylabel='Consumption, kWh'
         )
 
-    consumptionfig.legend((p1[0], p2[0]), ('Nonshiftable appliances',
-                                           'Shiftable appliances'))
+    handles, labels = consumptionfig.get_legend_handles_labels()
+    consumptionfig.legend(handles, labels)
 
     #Making the figure pretty
     consumptionfig.tick_params(axis="both", which="both", bottom="off",
@@ -43,36 +54,21 @@ def consumption_plot(shift, nonshift):
     consumptionfig.set_axisbelow(True)
     consumptionfig.grid(b=True, which='major', axis='y', color='#cccccc',
                         linestyle='--')
-    plt.tight_layout()
-    plt.show()
+    pricefig = consumptionfig.twinx()
+    pricefig.plot(price, color='black')
+    pricefig.set(ylabel='Price, NOK/kWh')
 
 
-
-def plot_price(price):
-    f, (pricefig) = plt.subplots(1, 1)
-    pricefig.set(
-        title='Price of energy in neighborhood',
-        xlabel='Hour',
-        ylabel='Price, NOK/kWh'
-    )
-
-    pricefig.plot(price)
-    # Making the figure pretty
-    pricefig.tick_params(axis="both", which="both", bottom="off",
-                               top="off", labelbottom="on", left="off",
-                               right="off", labelleft="on")
-    pricefig.spines["top"].set_visible(False)
-    pricefig.spines["bottom"].set_visible(False)
-    pricefig.spines["right"].set_visible(False)
-    pricefig.spines["left"].set_visible(False)
-    pricefig.set_axisbelow(True)
-    pricefig.grid(b=True, which='major', axis='y', color='#cccccc',
-                        linestyle='--')
     plt.tight_layout()
     plt.show()
 
 
 if __name__ == "__main__":
-    shiftable = np.random.rand(24)
-    nonshiftable = np.random.rand(24)
-    consumption_plot(shiftable, nonshiftable)
+    shiftable = [np.random.rand(24), np.random.rand(24)]
+    nonshiftable = [np.random.rand(24), np.random.rand(24)]
+    shiftablenames = ['shiftable', 'shiftable']
+    nonshiftablenames = ['nonshiftable', 'nonshiftable']
+    price = np.random.rand(24)
+    consumption_plot(shiftable, nonshiftable, shiftablenames,
+                     nonshiftablenames, price)
+
